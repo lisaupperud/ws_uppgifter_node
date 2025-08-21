34dcbcaa20192a094c2de2ef594e0b99d5f811f7
liluppis <lisa.upp@hotmail.com>
2025-08-21 10:45:46 +0200

    add logic for datatbase, add validation for secret

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,8 @@ const app = express()
 const port: number = 3000
 
 app.get("/", (req, res) => {
-  res.status(200).send("Hello World!")
+  res.status(200).send("Hello World!") // automatically converts to datatypes
+  res.json() // manually converts to JSON
 })
 
 app.listen(port, () => {
