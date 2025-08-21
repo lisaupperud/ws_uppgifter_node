332853d9a273f3b69a3c76c892e4a3b189265706
liluppis <lisa.upp@hotmail.com>
2025-08-21 10:51:11 +0200

    add startServer() to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import express from "express"
+import "dotenv/config" // oneliner for config
+import { closeDB, runDB } from "./db/database.js"
 
 const app = express()
 const port: number = 3000
@@ -11,3 +13,22 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
 })
+
+async function startServer() {
+  try {
+    await runDB()
+    app.listen(port, () => {
+      console.log(`Listening to port ${port}`)
+      console.log(`Start the app: http://localhost:${port}`)
+    })
+    process.on("SIGINT", async () => {
+      console.log("Cleaning up...")
+      await closeDB()
+      process.exit(0)
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+startServer()
