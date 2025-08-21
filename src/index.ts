41ea75c0642aadbd6067c9ef3889a634884fd2ca
liluppis <lisa.upp@hotmail.com>
2025-08-21 11:39:47 +0200

    add POST endpoint

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,17 @@ import { closeDB, runDB } from "./db/database.js"
 const app = express()
 const port: number = 3000
 
+// middleware to parse JSON request bodies
+// app.use(express.json())
+
+// define a User interface
+interface User {
+  id: number
+  name: string
+  email: string
+}
+
+// GET endpoint
 app.get("/:id", (req: Request, res: Response) => {
   const id: number = Number(req.params.id) // casting
 
@@ -14,6 +25,24 @@ app.get("/:id", (req: Request, res: Response) => {
   }
 
   res.send({ id: id })
+})
+
+// POST endpoint
+app.post("/user", (req: Request, res: Response) => {
+  const { name, email } = req.body
+
+  if (!name || !email) {
+    res.status(400).send({ message: "Name and email are required " })
+    return
+  }
+
+  const newUser: User = {
+    id: Math.floor(Math.random() * 1000),
+    name,
+    email,
+  }
+
+  res.status(201).send(newUser)
 })
 
 app.listen(port, () => {
